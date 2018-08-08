c38a31c872ba24ae6316a5f53a577ae664ed445c
altbdoor <lancersupraskyline@gmail.com>
2018-08-08 13:42:12 +0800

    micro optimization

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+        console.log('[PWA Builder] Cached index page during Install '+ response.url);
         return cache.put(indexPage, response);
       });
   }));
@@ -17,7 +17,7 @@ self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
     return caches.open('pwabuilder-offline').then(function (cache) {
       return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
+        console.log('[PWA Builder] add page to offline '+response.url)
         return cache.put(request, response);
       });
     });
